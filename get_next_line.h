80df4f8da80102e256c3d4ad1d3e7dd285afd9cd
ELBethan <monpseudojeux@gmail.com>
2023-11-15 17:04:56 +0100

    start

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: eburnet <eburnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:30:57 by eburnet           #+#    #+#             */
-/*   Updated: 2023/11/15 16:32:55 by eburnet          ###   ########.fr       */
+/*   Updated: 2023/11/15 16:43:13 by eburnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-
+char	*get_next_line(int fd);
 
 #endif
